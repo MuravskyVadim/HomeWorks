48d95a18b4ad87fba59fca0abeda19d614a272ae
MuravskyVadim <myrvadim@gmail.com>
2019-05-30 16:41:39 +0300

    Added Application at the end of the class name. Fixed other simple issues.

--- src/SecondHomeWorkApplication.java
+++ src/SecondHomeWorkApplication.java
@@ -1,4 +1,4 @@
-public class SecondHomeWork {
+public class SecondHomeWorkApplication {
     public static void main(String[] args) {
         System.out.println(booleanExpression(false, false, false, false));
         System.out.println(booleanExpression(true, true, true, true));
@@ -12,31 +12,26 @@ public class SecondHomeWork {
         System.out.println(leapYearCount(1));
         System.out.println(leapYearCount(4));
         System.out.println(leapYearCount(100));
-        System.out.println(leapYearCount(101));
-        System.out.println(leapYearCount(400));
-        System.out.println(leapYearCount(2016));
 
-        System.out.println(doubleExpression(0.1,0.2,0.3));
-        System.out.println(doubleExpression(0.0001,0.0002,0.0003));
-        System.out.println(doubleExpression(0.0001,-0.0002,0.0003));
+        System.out.println(doubleExpression(0.1, 0.2, 0.3));
+        System.out.println(doubleExpression(0.0001, 0.0002, 0.0003));
 
         System.out.println(flipBit(13, 1));
         System.out.println(flipBit(13, 2));
         System.out.println(flipBit(13, 3));
         System.out.println(flipBit(13, 4));
-
     }
 
     public static boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
-        return (((a ^ b) & (c ^ d)) | ((a ^ c) & (b ^ d)));
+        return ((a ^ b) & (c ^ d)) | ((a ^ c) & (b ^ d));
     }
 
     public static int leapYearCount(int year) {
-        return (year+(year/100)-(year/100)*4)/4;
+        return year / 4 - year / 100 + year / 400;
     }
 
     public static boolean doubleExpression(double a, double b, double c) {
-        return Math.round(a*10000) + Math.round(b*10000) == Math.round(c*10000);
+        return Math.abs((a + b) - c) <= 0.0001;
     }
 
     public static int flipBit(int value, int bitIndex) {

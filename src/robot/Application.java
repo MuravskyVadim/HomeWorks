d88931745de6e50b8890fcb5a6de6de35be71804
MuravskyVadim <myrvadim@gmail.com>
2019-05-31 13:01:24 +0300

    Fixed some issues

--- src/robot/Application.java
+++ src/robot/Application.java
@@ -11,49 +11,48 @@ public class Application {
         Direction direction = robot.getDirection();
         int x = robot.getX();
         int y = robot.getY();
-        if (toX > -1 && toY > -1) {
-            if (toX > robot.getX()) {
-                while (direction != Direction.RIGHT) {
-                    robot.turnLeft();
-                    direction = robot.getDirection();
-                }
 
-                while (toX != x) {
-                    robot.stepForward();
-                    x = robot.getX();
-                }
-            } else if (toX < robot.getX()) {
-                while (direction != Direction.LEFT) {
-                    robot.turnLeft();
-                    direction = robot.getDirection();
-                }
-
-                while (toX != x) {
-                    robot.stepForward();
-                    x = robot.getX();
-                }
+        if (toX > robot.getX()) {
+            while (direction != Direction.RIGHT) {
+                robot.turnLeft();
+                direction = robot.getDirection();
             }
 
-            if (toY > robot.getY()) {
-                while (direction != Direction.UP) {
-                    robot.turnLeft();
-                    direction = robot.getDirection();
-                }
+            while (toX != x) {
+                robot.stepForward();
+                x = robot.getX();
+            }
+        } else if (toX < robot.getX()) {
+            while (direction != Direction.LEFT) {
+                robot.turnLeft();
+                direction = robot.getDirection();
+            }
 
-                while (toY != y) {
-                    robot.stepForward();
-                    y = robot.getY();
-                }
-            } else if (toY < robot.getY()) {
-                while (direction != Direction.DOWN) {
-                    robot.turnLeft();
-                    direction = robot.getDirection();
-                }
+            while (toX != x) {
+                robot.stepForward();
+                x = robot.getX();
+            }
+        }
 
-                while (toY != y) {
-                    robot.stepForward();
-                    y = robot.getY();
-                }
+        if (toY > robot.getY()) {
+            while (direction != Direction.UP) {
+                robot.turnLeft();
+                direction = robot.getDirection();
+            }
+
+            while (toY != y) {
+                robot.stepForward();
+                y = robot.getY();
+            }
+        } else if (toY < robot.getY()) {
+            while (direction != Direction.DOWN) {
+                robot.turnLeft();
+                direction = robot.getDirection();
+            }
+
+            while (toY != y) {
+                robot.stepForward();
+                y = robot.getY();
             }
         }
     }

a37fcbb7c711b552be576acc1d8bc8b1c3b86694
MuravskyVadim <myrvadim@gmail.com>
2019-06-01 14:24:56 +0300

    HomeWork4 fix4 (delete imports)

--- src/PalindromeApp.java
+++ src/PalindromeApp.java
@@ -1,6 +1,3 @@
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class PalindromeApp {
     public static void main(String[] args) {
         System.out.println(isPalindrome("Madam, I'm Adam!"));

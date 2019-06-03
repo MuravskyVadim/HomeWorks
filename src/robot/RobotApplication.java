3fc87dec9e84ca05301b7566aebdec97dbef8dfd
MuravskyVadim <myrvadim@gmail.com>
2019-06-03 16:58:44 +0300

    HomeWork5 fix3

--- src/robot/RobotApplication.java
+++ src/robot/RobotApplication.java
@@ -3,18 +3,28 @@ package robot;
 public class RobotApplication {
     public static void moveRobot(RobotConnectionManager robotConnectionManager, int toX, int toY) {
         boolean connection = false;
+        RobotConnection robotConnection = null;
 
         for (int i = 0; !connection && i < 3; i++) {
-            try (RobotConnection robotConnection = robotConnectionManager.getConnection()){
+            try {
+                robotConnection = robotConnectionManager.getConnection();
                 robotConnection.moveRobotTo(toX, toY);
                 connection = true;
             } catch (RobotConnectionException e) {
                 e.getMessage();
+            } finally {
+                if (robotConnection != null) {
+                    try {
+                        robotConnection.close();
+                    } catch (RobotConnectionException e) {
+                        e.getMessage();
+                    }
+                }
             }
-        }
 
-        if (!connection) {
-            throw new RobotConnectionException("Connection failed");
+            if (!connection) {
+                throw new RobotConnectionException("Connection failed");
+            }
         }
     }
 }

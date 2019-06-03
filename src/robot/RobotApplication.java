13838440713a531237d825e7298a2b9b5090dd55
MuravskyVadim <myrvadim@gmail.com>
2019-06-03 16:54:42 +0300

    HomeWork5 fix 2

--- src/robot/RobotApplication.java
+++ src/robot/RobotApplication.java
@@ -1,36 +1,15 @@
 package robot;
 
 public class RobotApplication {
-    public static void main(String[] args) {
-        RobotConnectionManager robotConnectionManager = new RobotConnectionManager() {
-            @Override
-            public RobotConnection getConnection() {
-                return null;
-            }
-        };
-
-        moveRobot(robotConnectionManager, 1, 2);
-    }
-
     public static void moveRobot(RobotConnectionManager robotConnectionManager, int toX, int toY) {
-        RobotConnection robotConnection = null;
         boolean connection = false;
 
         for (int i = 0; !connection && i < 3; i++) {
-            try {
-                robotConnection = robotConnectionManager.getConnection();
+            try (RobotConnection robotConnection = robotConnectionManager.getConnection()){
                 robotConnection.moveRobotTo(toX, toY);
                 connection = true;
             } catch (RobotConnectionException e) {
                 e.getMessage();
-            } finally {
-                if (robotConnection != null) {
-                    try {
-                        robotConnection.close();
-                    } catch (RobotConnectionException e) {
-                        e.getMessage();
-                    }
-                }
             }
         }
 

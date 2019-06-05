78648863cff2b0c4aaea2dc19668c7886168c4ed
MuravskyVadim <myrvadim@gmail.com>
2019-06-05 15:12:58 +0300

    HomeWork7 fix1

--- src/Storage.java
+++ src/Storage.java
@@ -1,31 +1,48 @@
-public class Storage<T, K> {
-    private static final int ARRAY_LENGTH = 10;
-    private T[] keysArray;
-    private K[] valuesArray;
+public class Storage<K, V> {
+    private static final int ARRAY_LENGTH = 3;
+    private K[] keysArray;
+    private V[] valuesArray;
 
     public Storage() {
-        keysArray = (T[]) new Object[ARRAY_LENGTH];
-        valuesArray = (K[]) new Object[ARRAY_LENGTH];
+        keysArray = (K[]) new Object[ARRAY_LENGTH];
+        valuesArray = (V[]) new Object[ARRAY_LENGTH];
     }
 
-    public K get(T key) {
-        for (int i = 0; i < ARRAY_LENGTH; i++) {
-            if (keysArray[i].equals(key)) {
+    public V get(K key) {
+        for (int i = 0; i < keysArray.length; i++) {
+            if (key.equals(keysArray[i])) {
                 return valuesArray[i];
             }
         }
         return null;
     }
 
-    public void put(T key, K value) {
+    public void put(K key, V value) {
         if (key != null && value != null) {
-            for (int i = 0; i < ARRAY_LENGTH; i++) {
+            for (int i = 0; i < keysArray.length; i++) {
                 if ((keysArray[i] == null && valuesArray[i] == null) || key.equals(keysArray[i])) {
                     keysArray[i] = key;
                     valuesArray[i] = value;
                     break;
+                }else if(i == keysArray.length-1){
+                    increaseSize();
+                    i = 0;
                 }
             }
         }
+    }
+
+    public void increaseSize() {
+        int newArrayLength = keysArray.length + (keysArray.length >> 1);
+        K[] newK = (K[]) new Object[newArrayLength];
+        V[] newV = (V[]) new Object[newArrayLength];
+
+        for (int i = 0; i < keysArray.length; i++) {
+            newK[i] = keysArray[i];
+            newV[i] = valuesArray[i];
+        }
+
+        keysArray = newK;
+        valuesArray = newV;
     }
 }

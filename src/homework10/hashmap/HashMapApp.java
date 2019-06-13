03a633532da7e7549251e6a50715af0f74adc536
MuravskyVadim <myrvadim@gmail.com>
2019-06-13 22:36:49 +0300

    HomeWork10 deleted comments in HashMapApp class

--- src/homework10/hashmap/HashMapApp.java
+++ src/homework10/hashmap/HashMapApp.java
@@ -6,10 +6,10 @@ public class HashMapApp {
         hashMap.put(1, "Ivanov");
         hashMap.put(2, "Petrov");
         hashMap.put(3, "Sidorov");
-        hashMap.put(4, "Vostrikov");
+        hashMap.put(null, "Vostrikov");
         hashMap.put(5, "Mironov");
         hashMap.put(6, "Samofalov");
-        hashMap.put(7, "Efimov");
+        hashMap.put(null, "Efimov");
         hashMap.put(8, "Volkov");
         hashMap.put(9, "Sidorov");
         hashMap.put(9, "Andreev");
@@ -22,20 +22,14 @@ public class HashMapApp {
         System.out.println(hashMap.get(1));
         System.out.println(hashMap.get(2));
         System.out.println(hashMap.get(3));
-        System.out.println(hashMap.get(4));
+        System.out.println(hashMap.get(null));
         System.out.println(hashMap.get(5));
         System.out.println(hashMap.get(6));
-        System.out.println(hashMap.get(7));
         System.out.println(hashMap.get(8));
         System.out.println(hashMap.get(9));
         System.out.println(hashMap.get(10));
         System.out.println(hashMap.get(11));
         System.out.println(hashMap.get(12));
         System.out.println(hashMap.get(13));
-
-//        for (int i = 1; i <= hashMap.size(); i++) {
-//            System.out.println(hashMap.get(i));
-//        }
-//        System.out.println(hashMap);
     }
 }
